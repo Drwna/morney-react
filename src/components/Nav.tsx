dfb45951764230fd771a7277ac67879f340ecb85
draganet <3417237276@qq.com>
2022-01-23 00:06:39 +0800

    完成导航栏 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import 'icons/money.svg';
+import 'icons/labels.svg';
+import 'icons/statistics.svg';
 
 const NavWrapper = styled.div`
   line-height: 24px;
@@ -12,7 +15,16 @@ const NavWrapper = styled.div`
     > li {
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
     }
   }
 `;
@@ -22,12 +34,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#labels"/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
