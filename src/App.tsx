2304b2ab0d58f8c73a95fcddf8ca6c798fbce638
draganet <3417237276@qq.com>
2022-01-23 01:15:16 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,19 @@ import {
   Route,
   Navigate
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from 'components/Layout';
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Routes>
-            <Route path="/tags" element={<Tags/>}/>
-            <Route path="/money" element={<Money/>}/>
-            <Route path="/statistics" element={<Statistics/>}/>
-            <Route path="/" element={<Navigate replace to="/money"/>}/>
-            <Route path="*" element={<NotFound/>}/>
-          </Routes>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Routes>
+        <Route path="/tags" element={<Tags/>}/>
+        <Route path="/money" element={<Money/>}/>
+        <Route path="/statistics" element={<Statistics/>}/>
+        <Route path="/" element={<Navigate replace to="/money"/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </Router>
   );
 }
@@ -44,15 +27,27 @@ function NotFound() {
 }
 
 function Tags() {
-  return <h2>标签页</h2>;
+  return (
+    <Layout>
+      <h2>标签页</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页</h2>;
+  return (
+    <Layout>
+      <h2>记账页</h2>
+    </Layout>
+  );
 }
 
 function Statistics() {
-  return <h2>统计页</h2>;
+  return (
+    <Layout>
+      <h2>统计页</h2>
+    </Layout>
+  );
 }
 
 export default App;
