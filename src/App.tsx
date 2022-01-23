a556f3cb4b6e3da2805665f7b6b9c38d28ad9817
draganet <3417237276@qq.com>
2022-01-23 14:49:58 +0800

    完成 Money 页面的 TagSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,25 @@ import Labels from 'views/labels';
 import Money from 'views/Money';
 import Statistics from 'views/Statistics';
 import NotFound from './views/NotFound';
+import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/labels" element={<Labels/>}/>
-        <Route path="/money" element={<Money/>}/>
-        <Route path="/statistics" element={<Statistics/>}/>
-        <Route path="/" element={<Navigate replace to="/money"/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route path="/labels" element={<Labels/>}/>
+          <Route path="/money" element={<Money/>}/>
+          <Route path="/statistics" element={<Statistics/>}/>
+          <Route path="/" element={<Navigate replace to="/money"/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
 
