16bf44ee00a07fcc117c46892f25eb59970c7457
draganet <3417237276@qq.com>
2022-01-23 16:47:53 +0800

    完成 Money 页面的 数字面板 UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -82,7 +82,66 @@ const CategorySection = styled.section`
 `;
 
 const NumberSection = styled.section`
+  display: flex;
+  flex-direction: column;
 
+  > .output {
+    background: white;
+    font-size: 36px;
+    font-weight: bolder;
+    text-align: right;
+    line-height: 72px;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px  rgba(0, 0, 0, 0.3),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.3);
+  }
+
+  > .pad {
+    > button {
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+
+      &:nth-child(2), &:nth-child(5) {
+        background: #E0E0E0;
+      }
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+        background: #D3D3D3;
+      }
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+        background: #C1C1C1;
+      }
+
+
+      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+        background: #B8B8B8;
+      }
+
+      &:nth-child(14) {
+        background: #A9A9A9;
+      }
+
+      &:nth-child(12) {
+        background: #9A9A9A;
+      }
+    }
+  }
 `;
 
 
@@ -114,10 +173,10 @@ function Money() {
       </CategorySection>
 
       <NumberSection>
-        <div>
+        <div className="output">
           100
         </div>
-        <div className="buttons">
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -129,8 +188,8 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberSection>
