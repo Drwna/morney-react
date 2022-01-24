0c9051bb5a3edf30ca77a737e01875e9e81637be
draganet <3417237276@qq.com>
2022-01-25 02:29:59 +0800

    封装 findTag 到 useTags 里

--- src/views/Label.tsx
+++ src/views/Label.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {NavLink, useParams} from 'react-router-dom';
+import {useTags} from '../useTags';
+import NotFound from './NotFound';
 
+
+type Params = {
+  id: string
+}
 const Label: React.FC = () => {
+  const {findTag} = useTags();
+  const {id} = useParams<Params>();
+  let tag;
+  if (id !== undefined) tag = findTag(parseInt(id));
 
   return (
     <div>
-      yes
+      {tag ? tag.name : <NavLink to="/"><NotFound/></NavLink>}
     </div>
   );
 };
